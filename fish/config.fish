4e64c395e48ee2b63d8c19139a4f6f1d0f82bc98
m154k1 <139042094+m154k1@users.noreply.github.com>
2024-03-25 13:08:18 +0200

    .

---
+++ fish/config.fish
@@ -0,0 +1,19 @@
+if status is-interactive
+    fish_config theme choose "Base16 Default Dark"
+
+    set -g fish_greeting
+
+    set -gx EDITOR nvim
+    set -gx PAGER moar
+
+    set -gx XDG_CONFIG_HOME ~/.config
+    set -gx XDG_DATA_HOME ~/.local/share
+
+    # set -gx ANDROID_HOME ~/Library/Android/sdk
+    # set -gx JAVA_HOME "/Applications/Android Studio.app/Contents/jbr/Contents/Home"
+    # fish_add_path -gaP $JAVA_HOME/bin $ANDROID_HOME/{platform-tools,emulator}
+
+    eval (/opt/homebrew/bin/brew shellenv)
+
+    fish_add_path -gp ~/.local/bin
+end
---
+++ nvim/init.vim
@@ -0,0 +1,37 @@
+set nocompatible
+
+filetype indent on
+
+set ff=unix
+set ffs=unix,dos
+
+set history=10
+
+syntax on
+set showmatch
+
+set wildmenu
+
+set incsearch
+set hlsearch
+
+set wrap
+set linebreak
+set nolist
+set modeline
+
+set number
+
+set tabstop=4
+set softtabstop=4
+set shiftwidth=4
+set expandtab
+set smarttab
+
+set list
+set listchars=trail:·
+
+set shada="NONE"
+set noswapfile
+
+let g:netrw_dirhistmax = 0
