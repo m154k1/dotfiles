5446d69441496a8f917fb76b86b62602492326ba
m154k1 <139042094+m154k1@users.noreply.github.com>
2024-06-30 18:20:54 +0300

    .

---
+++ firefox/user-overrides.js
@@ -0,0 +1,39 @@
+user_pref("permissions.default.geo", 2);
+user_pref("permissions.default.desktop-notification", 2);
+
+user_pref("intl.accept_languages", "en-US,en,uk,ru");
+user_pref("font.default.x-western", "sans-serif");
+user_pref("font.default.x-cyrillic", "sans-serif");
+
+user_pref("cookiebanners.service.mode", 2);
+user_pref("cookiebanners.service.mode.privateBrowsing", 2);
+
+user_pref("general.autoScroll", false);
+user_pref("browser.tabs.allowTabDetach", false);
+user_pref("browser.tabs.tabmanager.enabled", false);
+user_pref("browser.urlbar.decodeURLsOnCopy", true);
+user_pref("findbar.highlightAll", true);
+user_pref("browser.shell.checkDefaultBrowser", false);
+user_pref("browser.compactmode.show", true);
+user_pref("media.videocontrols.picture-in-picture.video-toggle.enabled", false);
+user_pref("browser.urlbar.suggest.openpage", false);
+user_pref("browser.urlbar.suggest.engines", false);
+user_pref("browser.urlbar.suggest.topsites", false);
+
+user_pref("browser.warnOnQuit", false);
+user_pref("browser.warnOnQuitShortcut", false);
+user_pref("full-screen-api.warning.delay", -1);
+user_pref("full-screen-api.warning.timeout", 0);
+user_pref("security.dialog_enable_delay", 0);
+user_pref("browser.download.panel.shown", true);
+user_pref("browser.contentblocking.report.lockwise.enabled", false);
+user_pref("browser.contentblocking.report.show_mobile_app", false);
+
+user_pref("identity.fxaccounts.enabled", false);
+user_pref("browser.safebrowsing.malware.enabled", false);
+user_pref("browser.safebrowsing.phishing.enabled", false);
+user_pref("browser.tabs.firefox-view", false);
+user_pref("browser.preferences.moreFromMozilla", false);
+user_pref("extensions.pocket.enabled", false);
+user_pref("browser.newtabpage.activity-stream.feeds.topsites", false);
+user_pref("browser.privatebrowsing.vpnpromourl", "");
